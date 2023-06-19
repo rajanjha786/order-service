2cbc1a9477962928b9eccef60e76f1d82d342254
rajanjha786 <rajanjha786786@gmail.com>
2023-06-19 21:09:15 +0530

    Added Observability, Monitoring, and Logging changes

--- src/main/java/com/bookshop/orderservice/web/OrderController.java
+++ src/main/java/com/bookshop/orderservice/web/OrderController.java
@@ -4,6 +4,7 @@ import com.bookshop.orderservice.domain.order.Order;
 import com.bookshop.orderservice.domain.order.OrderService;
 import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
@@ -11,12 +12,14 @@ import reactor.core.publisher.Mono;
 @RestController
 @RequestMapping("orders")
 @AllArgsConstructor
+@Slf4j
 public class OrderController {
 
     private final OrderService orderService;
 
     @GetMapping
     public Flux<Order> getAllOrders() {
+        log.info("Getting all Orders");
         return orderService.getAllOrders();
     }
 
@@ -24,6 +27,7 @@ public class OrderController {
     public Mono<Order> submitOrder(
             @RequestBody @Valid OrderRequest orderRequest
     ) {
+        log.info("Received order {}", orderRequest);
         return orderService.submitOrder(
                 orderRequest.isbn(), orderRequest.quantity()
         );
